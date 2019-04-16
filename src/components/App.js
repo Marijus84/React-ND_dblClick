8c7c0649d22824fdbbf24fc57b393acb28ab6eaa
“Marijus84” <marijus.romanovas@hotmail.com>
2019-04-16 16:31:01 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,117 +1,108 @@
 import React from 'react';
 import Card from './Card';
 import axios from 'axios';
-import {endpoints, getImageUrl} from '../../config';
+import { endpoints, getImageUrl } from '../../config';
 
 class App extends React.Component {
-    constructor() {
-        super();
+  state = {
+    movieList: [],
+    genreList: [],
+    genreMovies: [],
+    favorites: [],
+    selectedGenre: null,
+  };
 
-        this.state = {
-            movieList: [],
-            genreList: [],
-            genreMovies: [],
-            selectedGenre: null,
-        };
+  componentDidMount() {
+    this.getMovies();
+    this.getGenres();
+  }
 
-        this.getMovies();
-        this.getGenres();
-        this.getGenreMovies();
+  getMovies = () => {
+    axios
+      .get(endpoints.mostPopularMovies())
+      .then(res => this.setMovieList(res.data.results))
+      .catch(error => console.log(error));
+  };
+
+  getGenres = () => {
+    axios
+      .get(endpoints.genres())
+      .then(res => this.setGenreList(res.data.genres))
+      .catch(error => console.log(error));
+  };
+
+  getGenreMovies = id => {
+    axios
+      .get(endpoints.genreMovies(id))
+      .then(res => this.setGenreMovies(res.data.results))
+      .catch(error => console.log(error));
+  };
+
+  setMovieList = list => {
+    this.setState({
+      movieList: list,
+    });
+  };
+
+  setGenreList = list => {
+    this.setState({
+      genreList: list,
+    });
+  };
+
+  setGenreMovies = list => {
+    this.setState({
+      genreMovies: list,
+    });
+  };
+
+  toggleFavorite = id => {
+    const { favorites } = this.state;
+    if (favorites.includes(id)) {
+      this.setState({ favorites: favorites.filter(item => item !== id) });
+    } else {
+      this.setState({ favorites: [...favorites, id] });
     }
+  };
 
+  selectedGenre = id => this.setState({ selectedGenre: id });
 
-    getMovies = () => {
-        axios
-            .get(endpoints.mostPopularMovies())
-            .then((res) => this.setMovieList(res.data.results))
-            .catch((error) => console.log(error));
-    };
+  render() {
+    const { movieList, genreList, genreMovies, selectedGenre, favorites } = this.state;
+    const movies = selectedGenre ? genreMovies : movieList;
 
-    getGenres = () => {
-        axios
-            .get(endpoints.genres())
-            .then((res) => this.setGenreList(res.data.genres))
-            .catch((error) => console.log(error));
-    };
+    console.log('movies', movies);
 
-    getGenreMovies = (id) => {
-        axios
-            .get(endpoints.genreMovies(id))
-            .then((res) => this.setGenreMovies(res.data.results))
-            .catch((error) => console.log(error));
-    };
-
-    setMovieList = (list) => {
-        this.setState({
-            movieList: list,
-        });
-    };
-
-    setGenreList = (list) => {
-        this.setState({
-            genreList: list,
-        });
-    };
-
-    setGenreMovies = (list) => {
-        this.setState({
-            genreMovies: list,
-        });
-    };
-
-    selectedGenre = (id) => this.setState({selectedGenre: id});
-
-    flag = (string) => this.setState({flag: string});
-
-    render() {
-        const {movieList, genreList, genreMovies, selectedGenre} = this.state;
-
-        if (selectedGenre) {
-            return (
-                <div>
-                    {genreList.map((genre) => (
-                        <button onClick={() => {
-                            this.selectedGenre(genre.id);
-                            this.getGenreMovies(selectedGenre);
-                            this.flag('genre');}
-                        }>{genre.name}
-                        </button>
-                    ))}
-
-                    {genreMovies.map((listItem) => (
-                        <Card
-                            backgroundImage={getImageUrl(listItem.backdrop_path)}
-                            title={listItem.original_title}
-                            releaseDate={listItem.release_date}
-                            score={listItem.vote_average}
-                            votes={listItem.vote_count}
-                            description={listItem.overview}
-                        />
-                    ))}
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    {genreList.map((genre) => (
-                        <button onClick={() => {
-                            this.selectedGenre(genre.id);}
-                        }>{genre.name}
-                        </button>
-                    ))}
-                    {movieList.map((listItem) => (
-                        <Card
-                            backgroundImage={getImageUrl(listItem.backdrop_path)}
-                            title={listItem.original_title}
-                            releaseDate={listItem.release_date}
-                            score={listItem.vote_average}
-                            votes={listItem.vote_count}
-                            description={listItem.overview}
-                        />
-                    ))}
-                </div>)
-        }
-    }
+    return (
+      <div>
+        {genreList.map(({ id, name }) => (
+          <button
+            onClick={() => {
+              this.selectedGenre(id);
+              this.getGenreMovies(id);
+            }}
+            key={id}
+          >
+            {name}
+          </button>
+        ))}
+        {movies.map(listItem => (
+          <Card
+            key={listItem.id}
+            backgroundImage={getImageUrl(listItem.backdrop_path)}
+            title={listItem.original_title}
+            releaseDate={listItem.release_date}
+            score={listItem.vote_average}
+            votes={listItem.vote_count}
+            description={listItem.overview}
+            onFavoriteClick={this.toggleFavorite}
+            id={listItem.id}
+            isFavorite={favorites.includes(listItem.id)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,13 @@ import React from 'react';
 class Card extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       opened: false,
-        liked: false
-    }
+    };
   }
-  
-  render() {
 
+  render() {
     const {
       backgroundImage,
       title,
@@ -19,9 +17,12 @@ class Card extends React.Component {
       score,
       votes,
       description,
+      isFavorite,
+      onFavoriteClick,
+      id,
     } = this.props;
-    const { opened, liked } = this.state;
-    
+    const { opened } = this.state;
+
     return (
       <div className="card">
         <div
@@ -30,39 +31,28 @@ class Card extends React.Component {
             backgroundImage: `url('${backgroundImage}')`,
           }}
         />
-    
-        <div className="card__title">
-          {title}
-        </div>
-    
-        <div className="card__like"
-             onClick={() => this.setState({ liked: !liked })}>
-            <i className= {liked ? 'fa fa-heart' : ' fa fa-heart-o'} />
-        </div>
-    
-        <div className="card__subtitle">
-            <span>{releaseDate}</span>
-            <span>{score} ({votes} votes)</span>
-        </div>
-    
-        <div className="card-info">
-        <div
-          className="card-info__header"
-          onClick={() => this.setState({ opened: !opened })}>
-          Summary
-        </div>
-          
-          {opened
-            ? (
-              <div className="card-info__description">
-                {description}
-              </div>
-            )
-            : null
-          }
-    </div>
-</div>
 
+        <div className="card__title">{title}</div>
+
+        <div className="card__like" onClick={() => onFavoriteClick(id)}>
+          <i className={isFavorite ? 'fa fa-heart' : ' fa fa-heart-o'} />
+        </div>
+
+        <div className="card__subtitle">
+          <span>{releaseDate}</span>
+          <span>
+            {score} ({votes} votes)
+          </span>
+        </div>
+
+        <div className="card-info">
+          <div className="card-info__header" onClick={() => this.setState({ opened: !opened })}>
+            Summary
+          </div>
+
+          {opened ? <div className="card-info__description">{description}</div> : null}
+        </div>
+      </div>
     );
   }
 }
